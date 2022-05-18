e8da0f40447a6ef1b4138e5be731f40bee1bea75
shaho1090 <shaho.parvini@gmail.com>
2022-05-18 13:05:38 +0430

    main class for making pizza

---
+++ app/Services/Pizza/PizzaMaker.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Services\Pizza;
+
+
+use App\Services\DashesToCamelCase;
+use Tests\Unit\PlainPizzaTest;
+
+class PizzaMaker
+{
+    private array $request;
+    private PizzaInterface $pizza;
+
+    public function __construct(array $request)
+    {
+        $this->request = $request;
+    }
+
+    public function handle(): PizzaInterface
+    {
+        $this->makePizza();
+        $this->addSauce();
+
+        return $this->pizza;
+    }
+
+    private function makePizza()
+    {
+        $pizzaClassName = (new DashesToCamelCase())->convert($this->request['pizza']['type'],true);
+
+        $pizzaClassName = __NAMESPACE__."\\PizzaTypes\\".$pizzaClassName;
+
+        $this->pizza = new $pizzaClassName(new PlainPizza());
+    }
+
+    private function addSauce()
+    {
+        if(empty($this->request['pizza']['sauce'])) {
+            return;
+        }
+
+        $sauceClassName = (new DashesToCamelCase())->convert($this->request['pizza']['sauce'],true);
+
+        $sauceClassName = __NAMESPACE__."\\SauceTypes\\".$sauceClassName;
+
+        $this->pizza = new $sauceClassName($this->pizza);
+    }
+}
