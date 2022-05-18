3f3adb33031266a15e3ae0525c1096b15e5861ae
shaho1090 <shaho.parvini@gmail.com>
2022-05-18 13:03:14 +0430

    resouce for pizza

---
+++ app/Http/Resources/PizzaResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PizzaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'cost' => $this->cost(),
+            'description' => $this->description()
+        ];
+    }
+}
