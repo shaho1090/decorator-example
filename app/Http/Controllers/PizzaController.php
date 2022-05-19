50af0b62c11cdaa6cef8243b4a3934aede3c6a44
shaho1090 <shaho.parvini@gmail.com>
2022-05-19 16:12:52 +0430

    add notifications

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\PizzaResource;
+use App\Services\notification\Notifier;
 use App\Services\Pizza\PizzaMaker;
 use Illuminate\Http\Request;
 
@@ -12,8 +13,17 @@ class PizzaController extends Controller
     {
         $pizza = (new PizzaMaker($request->toArray()))->handle();
 
+        $notifications = [];
+
+        if(isset($request['broadcasting-channels'])){
+           $notifications = (new Notifier($request['broadcasting-channels']))->handle();
+        }
+
         return response()->json([
-           'data' =>  new PizzaResource($pizza)
+           'data' =>  [
+               'pizza' => new PizzaResource($pizza),
+               'notification' => $notifications
+           ]
         ]);
     }
 }
